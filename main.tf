5ddc32312a6b8839b29116f523e046f15f915e38
saijeshu <153511679+saijeshu@users.noreply.github.com>
2024-02-12 12:46:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-#resource "aws_vpc" "main" {
-  #cidr_block       = var.vpc_cidr
-  #enable_dns_hostnames = var.enable_dns_hostnames
-  #tags = merge(
-    #var.common_tags, 
-    #var.vpc_tags,
-    #{
-        #Name = local.name
-    #}
-  #)
-#}
+resource "aws_vpc" "main" {
+  cidr_block       = var.vpc_cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
+  tags = merge(
+    var.common_tags, 
+    var.vpc_tags,
+    {
+        Name = local.name
+    }
+  )
+}
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
